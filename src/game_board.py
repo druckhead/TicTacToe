882effd45d228d245c7ce22d041ade5bc06b3dbf
druckhead <daniel32raz@gmail.com>
2022-11-26 22:38:00 +0200

    add validations

--- src/game_board.py
+++ src/game_board.py
@@ -1,9 +1,19 @@
 def get_board_size() -> int:
-    size = int(input('> X/O: Enter your board size (ex: 3 for 3 rows & 3 columns): '))
+    while True:
+        size = input('> X/O: Enter your board size (ex: 3 for 3 rows & 3 columns)\n'
+                     '>>> ')
+        if size.isdigit() is False:
+            print('Use positive digits only.')
+        else:
+            size = int(size)
+            if not (1 <= size <= 9):
+                print('Enter a board size ranging from 1 to 9 (inclusive).')
+            else:
+                break
     return size
 
 
-def initialize_board(size: int) -> list[list[None]]:
+def initialize_board(size: int) -> list[list]:
     board: list[list[None]] = list()
     for row in range(size):
         board.append([])
@@ -18,15 +28,25 @@ def initialize_board(size: int) -> list[list[None]]:
 def pprint_board(board: list[list[str]]) -> str:
     board_str = ""
     len_board = len(board[0])
+    print(" ", end="")
+    for col in range(len_board):
+        print(f"   {col + 1}", end="")
+    print()
+
+    # ascii for 'A'
+    letter = 65
     for row in range(len_board):
-        board_str += f'{"-" * (len_board * 4 + 1)}\n'
+        board_str += f'  {"-" * (len_board * 4 + 1)}\n'
+        board_str += f"{chr(letter)} "
+        letter += 1
         for col in range(len_board):
             shape = " " if board[row][col] is None else board[row][col]
             board_str += f"| {shape} "
         board_str += "|\n"
-    board_str += f'{"-" * (len_board * 4 + 1)}\n'
+    board_str += f'  {"-" * (len_board * 4 + 1)}\n'
     return board_str
 
 
-# def display_current_board(board: list[list[str]]) -> None:
-#     pass
+def display_current_board(board: list[list[str]]) -> None:
+    print(pprint_board(board))
+    return

08a6e2f9b6f0a8b84d0a64d5abcef88b49c0f1f2
Ziv Attias <116979853+zivattias@users.noreply.github.com>
2022-12-04 09:13:23 +0200

    commentary: added

--- src/game_logic.py
+++ src/game_logic.py
@@ -1,7 +1,7 @@
 from src.game_turns import Shape, toggle_shape
 from copy import deepcopy
 
-
+# Check win in rows one by one:
 def check_win_row(board: list[list[str]], row: int, len_col: int) -> bool:
     for col in range(len_col - 1):
         if board[row][col] is None:
@@ -10,14 +10,14 @@ def check_win_row(board: list[list[str]], row: int, len_col: int) -> bool:
             return False
     return True
 
-
+# Goes over each row bool return and determines win for ALL rows:
 def check_win_rows(board: list[list[str]], num_rows: int) -> bool:
     for row in range(num_rows):
         if check_win_row(board, row, len_col=num_rows) is True:
             return True
     return False
 
-
+# Check win in column one by one:
 def check_win_col(board: list[list[str]], col: int, len_row: int) -> bool:
     for row in range(len_row - 1):
         if board[row][col] is None:
@@ -26,14 +26,14 @@ def check_win_col(board: list[list[str]], col: int, len_row: int) -> bool:
             return False
     return True
 
-
+# Goes over each column bool return and determines win for ALL columns:
 def check_win_cols(board: list[list[str]], num_cols: int) -> bool:
     for col in range(num_cols):
         if check_win_col(board, col, len_row=num_cols) is True:
             return True
     return False
 
-
+# Diagonoal win check top left-bottom right:
 def check_top_left_bottom_right_diagonal_win(board: list[list[str]], num_rows: int) -> bool:
     for row in range(num_rows - 1):
         for col in range(row, row + 1):
@@ -43,7 +43,7 @@ def check_top_left_bottom_right_diagonal_win(board: list[list[str]], num_rows: i
                 return False
     return True
 
-
+# Diagional win check top right-bottom left:
 def check_top_right_bottom_left_diagonal_win(board: list[list[str]], num_rows: int) -> bool:
     for row in range(num_rows - 1, 0, -1):
         for col in range(num_rows - 1 - row, num_rows - row):
@@ -53,12 +53,12 @@ def check_top_right_bottom_left_diagonal_win(board: list[list[str]], num_rows: i
                 return False
     return True
 
-
+# Combines both diagonals checks to one:
 def check_win_diagonals(board: list[list[str]], num_rows: int) -> bool:
     return check_top_right_bottom_left_diagonal_win(board, num_rows) \
            or check_top_left_bottom_right_diagonal_win(board, num_rows)
 
-
+# An altogether win check function that retrieves bool returns from all check functions:
 def check_win(board: list[list[str]], num_rows: int) -> bool:
     return check_win_rows(board, num_rows) \
            or check_win_cols(board, num_cols=num_rows) \
@@ -66,7 +66,7 @@ def check_win(board: list[list[str]], num_rows: int) -> bool:
 
 
 # **************************************************************************
-#                               GAME STUCK
+#                            GAME STUCK - BONUS
 # **************************************************************************
 
 def stuck(board: list[list[str]], len_board: int, curr_shape: str) -> int:
